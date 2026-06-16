1c76fa359a951c9bdd0fabfe564943ee54dd52d2
Aaron Rine <aaronrine@gmail.com>
2026-06-16 10:28:04 -0700

    add ticket model

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=20, default="open")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
