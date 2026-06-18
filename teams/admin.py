c178305ea5b4b59cd186ce57a1af9917d1aa0b42
Aaron Rine <aaronrine@gmail.com>
2026-06-18 13:55:15 -0700

    fix detail page not showing assignee, add admin support for teams

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Team, TeamMembership
+
+
+class TeamMembershipInline(admin.TabularInline):
+    model = TeamMembership
+    extra = 1
+    autocomplete_fields = ["user"]
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+    inlines = [TeamMembershipInline]
+
+
+@admin.register(TeamMembership)
+class TeamMembershipAdmin(admin.ModelAdmin):
+    list_display = ["team", "user", "role"]
+    list_filter = ["role", "team"]
+    search_fields = ["team__name", "user__username"]
+    autocomplete_fields = ["team", "user"]
